39a8d4b6fb79c469e96e2508ed01a42af94384a3
Kristy Li <kristyli20.0@gmail.com>
2020-11-13 11:06:00 -0500

    feat: Complete 83. Remove Duplicates from Sorted List

---
+++ 83.js
@@ -0,0 +1,13 @@
+const deleteDuplicates = (head) => {
+  let current = head;
+
+  while (current && current.next) {
+    if (current.val === current.next.val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return head;
+};
