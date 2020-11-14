a4aa754749bb12dbed9aced081115c63ab894b9c
Kristy Li <kristyli20.0@gmail.com>
2020-11-14 14:58:12 -0500

    feat: Complete 283. Move Zeroes

---
+++ 283.js
@@ -0,0 +1,16 @@
+const moveZeroes = (nums) => {
+  let j = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      const temp = nums[i];
+
+      nums[i] = nums[j];
+      nums[j] = temp;
+
+      j++;
+    }
+  }
+
+  return nums;
+};
