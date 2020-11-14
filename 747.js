37fd10b313c0fa94af6fb5a93096c197ad7be1eb
Kristy Li <kristyli20.0@gmail.com>
2020-11-14 08:48:59 -0500

    feat: Complete 747. Largest Number At Least Twice of Others

---
+++ 747.js
@@ -0,0 +1,33 @@
+const dominantIndex = (nums) => {
+  if (!nums.length) {
+    return -1;
+  }
+
+  if (nums.length === 1) {
+    return 0;
+  }
+
+  let indexOfMax;
+  let indexOfSecondMax;
+
+  if (nums[0] > nums[1]) {
+    indexOfMax = 0;
+    indexOfSecondMax = 1;
+  } else {
+    indexOfMax = 1;
+    indexOfSecondMax = 0;
+  }
+
+  for (let i = 2; i < nums.length; i++) {
+    const currentNum = nums[i];
+
+    if (currentNum > nums[indexOfMax]) {
+      indexOfSecondMax = indexOfMax;
+      indexOfMax = i;
+    } else if (currentNum > nums[indexOfSecondMax]) {
+      indexOfSecondMax = i;
+    }
+  }
+
+  return nums[indexOfMax] >= nums[indexOfSecondMax] * 2 ? indexOfMax : -1;
+};
