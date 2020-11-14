9d5d99e6eafdb026caa6dc06d9d39e3052363b60
Kristy Li <kristyli20.0@gmail.com>
2020-11-14 09:52:12 -0500

    feat: Complete 66. Plus One

---
+++ 66.js
@@ -0,0 +1,21 @@
+const plusOne = (digits) => {
+  if (!digits.length) {
+    return digits;
+  }
+
+  for (let i = digits.length - 1; i >= 0; i--) {
+    const currentDigit = digits[i];
+
+    if (currentDigit !== 9) {
+      digits[i]++;
+
+      return digits;
+    } else {
+      digits[i] = 0;
+    }
+  }
+
+  digits.unshift(1);
+
+  return digits;
+};
